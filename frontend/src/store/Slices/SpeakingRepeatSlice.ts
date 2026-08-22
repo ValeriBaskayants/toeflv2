2a3a23e4ceb9e0be5c5ef5584b4ddf5387360ce2
ValeriBaskayants <valeri.baskayanc.y@gmail.com>
2026-08-22 17:17:54 +0400

    Speaking module

--- frontend/src/store/Slices/SpeakingRepeatSlice.ts
+++ frontend/src/store/Slices/SpeakingRepeatSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { speakingRepeatApi } from '@/api/services/speaking-repeat';
 import type {
